9b9f94b6e8200d9f8930d459f0a8d8ee22637302
SSemar <ssemar12@gmail.com>
2025-01-03 22:12:00 -0600

    test

---
+++ backend/db/migrations/20250102224839-create-reviewimage.js
@@ -0,0 +1,32 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.createTable('ReviewImages', {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: 'Spots', key: 'id' }, // Assuming this is the relationship
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      });
+    },
+    down: async (queryInterface, Sequelize) => {
+      await queryInterface.dropTable('ReviewImages');
+    },
+  };
+  
