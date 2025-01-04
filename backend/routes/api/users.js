e1cb0e87d8fbdc1b53f40361ec050c8030de0c57
SSemar <ssemar12@gmail.com>
2025-01-04 17:54:48 -0600

    users fix?

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,16 +7,42 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie } = require('../../utils/auth');
 
 //! Validation
+// const validateSignup = [
+//   check('email')
+//     .exists({ checkFalsy: true })
+//     .isEmail()
+//     .withMessage('Please provide a valid email.'),
+//   check('username')
+//     .exists({ checkFalsy: true })
+//     .isLength({ min: 4 })
+//     .withMessage('Please provide a username with at least 4 characters.'),
+//   check('username')
+//     .not()
+//     .isEmail()
+//     .withMessage('Username cannot be an email.'),
+//   check('password')
+//     .exists({ checkFalsy: true })
+//     .isLength({ min: 6 })
+//     .withMessage('Password must be 6 characters or more.'),
+//   check('firstName')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a first name.'),
+//   check('lastName')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a last name.'),
+//   handleValidationErrors,
+// ];
+
+
 const validateSignup = [
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Invalid email'),
   check('username')
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
+    .withMessage('Please provide a username with at least 4 characters.')
     .not()
     .isEmail()
     .withMessage('Username cannot be an email.'),
@@ -26,10 +52,10 @@ const validateSignup = [
     .withMessage('Password must be 6 characters or more.'),
   check('firstName')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a first name.'),
+    .withMessage('First Name is required'),
   check('lastName')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a last name.'),
+    .withMessage('Last Name is required'),
   handleValidationErrors,
 ];
 
@@ -53,7 +79,7 @@ router.post(
 
     await setTokenCookie(res, safeUser);
 
-    return res.json({
+    return res.status(201).json({
       user: safeUser,
     });
   }
