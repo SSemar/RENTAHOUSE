9952a70b22462d9f18918000840797f6c3da889b
SSemar <ssemar12@gmail.com>
2025-01-03 22:36:49 -0600

    test1

---
+++ backend/db/migrations/20241219043755-add-firstName-lastName-to-users.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.addColumn('users', 'firstName', {
+            type: Sequelize.STRING,
+            allowNull: false,
+        });
+        await queryInterface.addColumn('users', 'lastName', {
+            type: Sequelize.STRING,
+            allowNull: false,
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.removeColumn('users', 'firstName');
+        await queryInterface.removeColumn('users', 'lastName');
+    }
+};
