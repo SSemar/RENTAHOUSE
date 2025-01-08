043226f53c6476cbfd0941ff72a14b1dd207022c
Sean <ssemar12@gmail.com>
2025-01-07 20:22:11 -0600

    autoFix

--- backend/db/migrations/20250103083617-create-spots.js
+++ backend/db/migrations/20250103083617-create-spots.js
@@ -80,7 +80,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
-    });
+    }, options);
   },
 
   async down(queryInterface, Sequelize) {
