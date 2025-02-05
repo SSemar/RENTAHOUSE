fc2785290ef49d62fc09d18a944ad45c280330cf
Sean <ssemar12@gmail.com>
2025-02-04 18:51:18 -0600

    2.4.25

--- backend/db/migrations/20250103083617-create-spots.js
+++ backend/db/migrations/20250103083617-create-spots.js
@@ -4,7 +4,9 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+ //! Add reviews to spots
 
+  
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Spots', {
