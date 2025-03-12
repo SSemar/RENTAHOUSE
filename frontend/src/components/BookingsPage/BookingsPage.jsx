06dd0212ecdb2d9b9f339f72655993b1417d714f
Sean <ssemar12@gmail.com>
2025-03-12 18:53:52 -0500

    test render

--- frontend/src/components/BookingsPage/BookingsPage.jsx
+++ frontend/src/components/BookingsPage/BookingsPage.jsx
@@ -1,22 +1,21 @@
-import './BookingsPage.css';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import * as bookingsActions from '../../store/bookings';
+// import './BookingsPage.css';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+// import * as bookingsActions from '../../store/bookings';
 
-//! fix this booking page? maybe has to have (state => state.bookings.allBookings); 
-// figure out the structure of the bookings reducer
-const BookingsPage = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+// //! fix this booking page? maybe has to have (state => state.bookings.allBookings); 
+// // figure out the structure of the bookings reducer
+// const BookingsPage = () => {
+//     const dispatch = useDispatch();
+//     const navigate = useNavigate();
     
-    useEffect(() => {
-        dispatch(bookingsActions.getBookings());
-        }, [dispatch]);
-    };
-    //! fix this
-    return (
-        <main className="bookings-main">
-            <h1>Manage Bookings</h1>
-        </main>
-    );
+//     useEffect(() => {
+//         dispatch(bookingsActions.getBookings());
+//         }, [dispatch]);
+//     };
+//     return (
+//         <main className="bookings-main">
+//             <h1>Manage Bookings</h1>
+//         </main>
+//     );
